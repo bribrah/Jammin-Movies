35e565be0ccbb5f11488e9a3aec22e7a842a23fe
bribrah <cheesemas46@gmail.com>
2019-04-04 16:57:03 -0700

    added CNAME

--- Web scraper/scraper.py
+++ Web scraper/scraper.py
@@ -1,58 +1,65 @@
 from bs4 import BeautifulSoup
+from selenium import webdriver 
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC 
+from selenium.common.exceptions import TimeoutException
 import requests
 import time
 ########################## NETFLIX #######################
+print("starting netflix")
 netflixLibraryURL = "https://www.finder.com/netflix-movies"
 
 pageResponse = requests.get(netflixLibraryURL, timeout=5)
 
 pageContent = BeautifulSoup(pageResponse.content, "html.parser")
 
-titleList = open("netflixtitles.txt", "w")
+titleList = open("Web scraper/netflixtitles.txt", "w")
 titleList.write("")
 titleList.close()
 
-titleList = open("netflixtitles.txt" , "a")
+titleList = open("Web scraper/netflixtitles.txt" , "a")
 
 titleArray = pageContent.find_all("b");
-
 
 for title in titleArray:
     titleList.write(title.text)
     titleList.write(",")
 titleList.close()
+print("netflix done")
 ############### HULU #################
-titleList = open("hulutitles.txt", "w")
+print("starting hulu")
+titleList = open("Web scraper/hulutitles.txt", "w")
 titleList.write("")
 titleList.close()
-titleList = open("hulutitles.txt", "a")
+titleList = open("Web scraper/hulutitles.txt", "a")
 titleArray = []
 
 for i in range(0,75):
     huluLibraryURL = "https://reelgood.com/source/hulu?filter-sort=4&offset=" + str(i*50)
     pageResponse = requests.get(huluLibraryURL, timeout=5)
     pageContent = BeautifulSoup(pageResponse.content, "html.parser")
-    titleContainerArray = pageContent.find_all("td", "cd")
+    titleContainerArray = pageContent.find_all("td", "c5")
     for container in titleContainerArray:
         titleArray.append(container.find("a").text)
-
 for title in titleArray:
     titleList.write(title)
     titleList.write(",")
 titleList.close()
-
+print("hulu done")
 ################### AMAZON #####################
-titleList = open("amazonTitles.txt", "w")
+print("starting amazon")
+titleList = open("Web scraper/amazonTitles.txt", "w")
 titleList.write("")
 titleList.close()
-titleList = open("amazonTitles.txt", "a")
+titleList = open("Web scraper/amazonTitles.txt", "a")
 titleArray = []
 
 for i in range(0,264):
     amazonURL = "https://reelgood.com/movies/source/amazon?filter-sort=4&offset=" + str(i*50)
     pageResponse = requests.get(amazonURL, timeout=5)
     pageContent = BeautifulSoup(pageResponse.content, "html.parser")
-    titleContainerArray = pageContent.find_all("td", "cd")
+    titleContainerArray = pageContent.find_all("td", "c5")
     for container in titleContainerArray:
         titleArray.append(container.find("a").text)
 
@@ -60,4 +67,4 @@ for title in titleArray:
     titleList.write(title)
     titleList.write(",")
 titleList.close()
-
+print("amazon done")
