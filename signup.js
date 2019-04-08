d78ef62fd38dfd9261afbae916b09884b38a0a36
bribrah <cheesemas46@gmail.com>
2019-04-08 00:13:15 -0700

    improved account creation

--- signup.js
+++ signup.js
@@ -3,24 +3,29 @@ const passwordInput = document.querySelector("#password");
 const passwordConfInput = document.querySelector("#password-conf");
 const errorsOutput = document.querySelector(".signup-errors")
 const submit = document.querySelector("#create-account");
+const firstList = document.querySelector("#first-list");
 const db = firebase.firestore();
 
 
 submit.addEventListener('click', createAccount);
 function createAccount(){
     if (passwordInput.value == passwordConfInput.value){
-        firebase.auth().createUserWithEmailAndPassword(emailInput.value, passwordInput.value).catch(function(error) {
+        firebase.auth().createUserWithEmailAndPassword(emailInput.value, passwordInput.value).then(()=>{
+            
+            errorsOutput.textContent = "Account Created."
+            db.collection(emailInput.value).doc("movie_lists").set({
+                movie_list_array: [firstList.value]
+            }).then(() =>{
+                firebase.auth().signInWithEmailAndPassword(emailInput.value, passwordInput.value).then(()=>{
+                    window.location.href = "index.html";
+                })
+            })
+        }).catch(function(error){
             // Handle Errors here.
             let errorCode = error.code;
             let errorMessage = error.message;
             errorsOutput.textContent = errorMessage;
             // ...
-        });
-        errorsOutput.textContent = "Account Created."
-        db.collection(emailInput.value).doc("movie_lists").set({
-            movie_list_array: []
-        }).then(() =>{
-            window.location.href = "index.html";
         })
     }
     else{
