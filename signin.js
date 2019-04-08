d3189ff835d3fb64f4c17c0323223ce25c150d47
bribrah <cheesemas46@gmail.com>
2019-04-08 15:37:01 -0700

    fixed home link

--- signin.js
+++ signin.js
@@ -40,7 +40,7 @@ function createUserObject(){
         document.querySelector("#logout").addEventListener('click', logout)
         console.log(window.location.pathname)
         
-        if (window.location.pathname == '/'){
+        if (window.location.pathname == '/' || window.location.pathname == "/index"){
             console.log("test")
             addMovieButton.disabled = false;
             populateListSelect();
